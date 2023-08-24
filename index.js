297b262ad0e4a8b426910f5e18b993b37bdb8ece
ItsBaZs112 <62480771+ItsBaZs112@users.noreply.github.com>
2023-08-24 14:54:43 -0700

    idk

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 
 var weapons = ["F. Storm","Rolling C.","H.Bomb","I.Slasher","T.Beam","S.Arm","T.Slow","O.Slider","M.Blade","L.Shield"]
+var weaponsIcons = [9999,irandom(22),irandom(22),irandom(22),irandom(22),irandom(22),irandom(22),irandom(22),irandom(22))
 var maxNumbers = 5
 var names = ["remastered","cut man","intro stage","level pack", "kaizo", "1-5", "protovania", "2023 revamped","roll","tutorial","wily stage","6","woman","man","mega man 12", "mega man 13", "mega man 10", "enker", "GB", "NES", "remake","challenge", "recreated", "recreation","demake","7","8","boss rush","crystal gate",Math.round(Math.random()),"kazoo","kiazo","fangame","yellow devil","nico evaluates","rockman and forte","the sequel","1_8_0","1_7_5","1_6_0","puzzle","neo cutman","contest","i wanna kill megaman","force beam","gimmick","contraption","illegal","factory","cutmna","hardman","concept", "mega man x","zero","mega man maker x","community maker","fortnite","joe biden","strike man","megaman","protoman","bass","roll","super hard","impossible","worlds hardest","easy","traditional","megaman 2","magnet","pluto","saturn","stardroid","battan","cossack","stage","airship","fire base","dark man","4","3","2","1","big pets","Ryu","sea",
 "v2","v3","v4","passage","entrance","skull","castle","gun","nrs","vui","feeber",randomWeapon(),"example level","prontoman","mega man","rockman","electro guard","speedrun","tech","glitch","what","a","leafshield","bielles","mmmx","modded",randomWeapon(),randomWeapon(),randomWeapon(),"-","-","-","-","-","-","-","-","-","-","-","-","wow","hard","ez","meka snack","go fast","apology level","b.dash","vs","the level","- ultimate edition","deluxe edition","and bass","dark man 5","fortress","castle","cut","intro","stage","12","13","i","wanna","kill","guard","if it was good","improvement","halloween","christmas","walk","finish","line","death","temple","DWN","dead","man","e","ballade","punk","gate","spam","burner man","big fish","stage","pronto man","heat ladder","quint","sunstar","palace","megamix","bpss","cossack","wily","steam man","meme","dead","bals"]
@@ -20,6 +21,9 @@ function randomWeapon() {
 }
 function randomMugshot() {
     return mugs[Math.floor(Math.random()*mugs.length)];  
+}
+function rngWPN() {
+    return weaponsIcons[Math.floor(Math.random()*weaponsIcons.length)];  
 }
 
 const randomCycle = [
@@ -56,18 +60,18 @@ function randomizeBase() {
         hasChoseHyphen = true
     }
     
-    w1.src = "res/ast.png"
-    w2.src = "res/ast.png"
-    w3.src = "res/ast.png"
-    w4.src = "res/ast.png"
-    w5.src = "res/ast.png"
-    w6.src = "res/ast.png"
-    w7.src = "res/ast.png"
-    w8.src = "res/ast.png"
-    w9.src = "res/ast.png"
-    w10.src = "res/ast.png"
-    w11.src = "res/ast.png"
-    w12.src = "res/ast.png"
+    w1.src = "res/weapons"+string(rngWPN())+".png"
+    w2.src = "res/weapons"+string(rngWPN())+".png"
+    w3.src = "res/weapons"+string(rngWPN())+".png"
+    w4.src = "res/weapons"+string(rngWPN())+".png"
+    w5.src = "res/weapons"+string(rngWPN())+".png"
+    w6.src = "res/weapons"+string(rngWPN())+".png"
+    w7.src = "res/weapons"+string(rngWPN())+".png"
+    w8.src = "res/weapons"+string(rngWPN())+".png"
+    w9.src = "res/weapons"+string(rngWPN())+".png"
+    w10.src = "res/weapons"+string(rngWPN())+".png"
+    w11.src = "res/weapons"+string(rngWPN())+".png"
+    w12.src = "res/weapons"+string(rngWPN())+".png"
     max = document.getElementById("nums").value;
     levelName = "";
     document.getElementById("mugshot").src = "res/"+randomMugshot()+".png"
