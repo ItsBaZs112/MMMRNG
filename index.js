6c25d9d55e915347f5439ada1fa09d42abbc054c
ItsBaZs112 <62480771+ItsBaZs112@users.noreply.github.com>
2023-08-25 11:25:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ return Math.round(num)
 }
 function rngWeapon() {
     var random = Math.random()*3
-    if (round(random) == 3)
+    if (round(random) != 3)
     return weaponIcons[Math.floor(Math.random()*weaponIcons.length)].toString();  
     else return "9999"
 }
