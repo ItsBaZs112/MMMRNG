a6bb51bf0ccb633aa2099b9a5e69b537e71ccaf4
ItsBaZs112 <62480771+ItsBaZs112@users.noreply.github.com>
2023-08-25 14:36:26 -0700

    Update index.js
    made the rng weapon system more flexible, now time to import everything in gml into here /j

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-var weaponIcons = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,9999]
+
+
+
 var maxNumbers = 5
 var names = ["remastered","cut man","intro stage","level pack", "kaizo", "1-5", "protovania", "2023 revamped","roll","tutorial","wily stage","6","woman","man","mega man 12", "mega man 13", "mega man 10", "enker", "GB", "NES", "remake","challenge", "recreated", "recreation","demake","7","8","boss rush","crystal gate",Math.round(Math.random()*13),"kazoo","kiazo","fangame","yellow devil","nico evaluates","rockman and forte","the sequel","1_8_0","1_7_5","1_6_0","puzzle","neo cutman","contest","i wanna kill megaman","force beam","gimmick","contraption","illegal","factory","cutmna","hardman","concept", "mega man x","zero","mega man maker x","community maker","fortnite","joe biden","strike man","megaman","protoman","bass","roll","super hard","impossible","worlds hardest","easy","traditional","megaman 2","magnet","pluto","saturn","stardroid","battan","cossack","stage","airship","fire base","dark man","4","3","2","1","big pets","Ryu","sea",
 "v2","v3","v4","passage","entrance","skull","castle","gun","nrs","vui","feeber","example level","prontoman","mega man","rockman","electro guard","speedrun","tech","glitch","what","a","leafshield","bielles","mmmx","modded","-","-","-","-","-","-","-","-","-","-","-","-","wow","hard","ez","meka snack","go fast","apology level","b.dash","vs","the level","- ultimate edition","deluxe edition","and bass","dark man 5","fortress","castle","cut","intro","stage","12","13","i","wanna","kill","guard","if it was good","improvement","halloween","christmas","walk","finish","line","death","temple","DWN","dead","man","e","ballade","punk","gate","spam","burner man","big fish","stage","pronto man","heat ladder","quint","sunstar","palace","megamix","bpss","cossack","wily","steam man","meme","dead","bals"]
@@ -22,6 +24,11 @@ function randomMugshot() {
 function round(num) {
 return Math.round(num)
 }
+function irandom(maximum) {
+    return round(Math.random()) * maximum
+    
+}
+var weaponIcons = [irandom(30),9999]
 function rngWeapon() {
     var random = Math.random()*3
     if (round(random) != 3)
